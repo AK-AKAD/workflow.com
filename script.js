a161cac9bd3394f43b191204c49dab362c5337b5
pricelessgift <144929332+pricelessgift@users.noreply.github.com>
2024-07-23 11:02:15 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,47 @@
+function addAnnouncement() {
+    const table = document.getElementById("announcementTable");
+    const announcement = document.getElementById("announcement").value;
+    const time = document.getElementById("time").value;
+    const add = document.getElementById("add").value;
+
+    if (announcement && time && add) {
+        const row = table.insertRow(-1);
+        const cell1 = row.insertCell(0);
+        const cell2 = row.insertCell(1);
+        const cell3 = row.insertCell(2);
+
+        cell1.textContent = announcement;
+        cell2.textContent = time;
+        cell3.textContent = add;
+
+        document.getElementById("announcement").value = '';
+        document.getElementById("time").value = '';
+        document.getElementById("add").value = '';
+    } else {
+        alert("Please fill all fields");
+    }
+}
+
+function addProject() {
+    const table = document.getElementById("projectsTable");
+    const title = document.getElementById("title").value;
+    const startDate = document.getElementById("startDate").value;
+    const endDate = document.getElementById("endDate").value;
+
+    if (title && startDate && endDate) {
+        const row = table.insertRow(-1);
+        const cell1 = row.insertCell(0);
+        const cell2 = row.insertCell(1);
+        const cell3 = row.insertCell(2);
+
+        cell1.textContent = title;
+        cell2.textContent = startDate;
+        cell3.textContent = endDate;
+
+        document.getElementById("title").value = '';
+        document.getElementById("startDate").value = '';
+        document.getElementById("endDate").value = '';
+    } else {
+        alert("Please fill all fields");
+    }
+}
