2fe5d3c147d8094da8a6a1c5b1837ea383e3f1c5
pricelessgift <144929332+pricelessgift@users.noreply.github.com>
2024-08-08 01:14:22 +0100

    added new js file (js/create.js) modifies html file(create.html, index.html) modifies css file (css/create.css, css/styles.css) modifies js files(js/checklist.js, js/script.js)

--- JS/checklist.js
+++ JS/checklist.js
@@ -1,24 +1,3 @@
-
-// document.querySelector('.filter button').addEventListener('click', function() {
-//     const filterText = document.querySelector('.filter input').value.toLowerCase();
-//     const rows = document.querySelectorAll('tbody tr');
-
-//     rows.forEach(row => {
-//         const cells = row.querySelectorAll('td');
-//         const name = cells[0].textContent.toLowerCase();
-//         const assignees = cells[1].textContent.toLowerCase();
-
-//         if (name.includes(filterText) || assignees.includes(filterText)) {
-//             row.style.display = '';
-//         } else {
-//             row.style.display = 'none';
-//         }
-//     });
-// });
-
-
-
-
 document.getElementById('addRowButton').addEventListener('click', function() {
     let table = document.getElementById('checklistTable').getElementsByTagName('tbody')[0];
     let newRow = table.insertRow();
