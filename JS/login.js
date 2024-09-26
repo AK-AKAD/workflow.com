02ae7d7c5e1b42a0fc17957f92f1624731e79c0e
pricelessgift <144929332+pricelessgift@users.noreply.github.com>
2024-09-26 21:57:24 +0100

    commit all modified html files commit all modified css files commit all modified js files

--- JS/login.js
+++ JS/login.js
@@ -4,6 +4,7 @@ var emailError = document.getElementById('email-error');
 var passwordError = document.getElementById('password-error');
 var conPasswordError = document.getElementById('conPassword-error');
 var submitError = document.getElementById('submit-error');
+var togglePasswordIcon = document.getElementById('.toggle-password');
 
 function validateName(){
     var name = document.getElementById('contact-name').value;
@@ -83,3 +84,17 @@ function validateForm(){
         return false;
     }
 }
+
+
+
+togglePasswordIcon.addEventListener('click', () => {
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text'; 
+        togglePasswordIcon.classList.remove('fa-eye');
+        togglePasswordIcon.classList.add('fa-eye-slash');  
+    } else {
+        passwordInput.type = 'password';  
+        togglePasswordIcon.classList.remove('fa-eye-slash');
+        togglePasswordIcon.classList.add('fa-eye');  
+    }
+});
